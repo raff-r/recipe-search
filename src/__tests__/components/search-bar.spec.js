cc8a07b378b6dbda60870298b3fd42a8827e9aee
Raffaele Rizzetti <hello@raffr.co.uk>
2017-06-10 00:01:26 +0100

    remove premature redux test code and switch to shallow render

--- src/__tests__/components/search-bar.spec.js
+++ src/__tests__/components/search-bar.spec.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import configureStore from 'redux-mock-store';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import toJson from "enzyme-to-json";
 
 import SearchBar from '../../components/search-bar';
 
 describe('SearchBar Component', () => {
 
-  const mockStore = configureStore();
-  const initialState = {};
-  let component, store;
+  let component;
 
   beforeEach(() => {
-    store = mockStore(initialState);
-    component = mount( <SearchBar /> );
+    component = shallow( <SearchBar /> );
   });
 
   it('should match the snapshot', () => {
     expect(toJson(component)).toMatchSnapshot();
-  })
+  });
 
 });
