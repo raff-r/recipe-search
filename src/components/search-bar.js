6dd6a94aa597e1f20143d053d28a106d0936e027
Raffaele Rizzetti <hello@raffr.co.uk>
2017-06-09 23:59:16 +0100

    mange input change state and form on submit

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -2,15 +2,47 @@ import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {searchTerm: ''};
+
+    this.onHandleChange = this.onHandleChange.bind(this);
+    this.onHandleSubmit = this.onHandleSubmit.bind(this);
+
+  }
+
+  onHandleChange(e) {
+    this.setState({searchTerm: e.target.value});
+  }
+
+  onHandleSubmit(e) {
+    e.preventDefault();
+    this.setState({searchTerm: ''});
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.onHandleSubmit}>
         <div className="form-group">
           <div className="input-group">
-            <input type="text" className="form-control" placeholder="Search&hellip;" />
+
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search&hellip;"
+              onChange={this.onHandleChange}
+              value={this.state.searchTerm}
+            />
+
             <span className="input-group-btn">
-              <button type="button" className="btn btn-default"><span className="glyphicon glyphicon-search"></span></button>
-          </span>
+              <button
+                type="button"
+                className="btn btn-default">
+                  <span className="glyphicon glyphicon-search" />
+              </button>
+            </span>
+            
           </div>
         </div>
       </form>
