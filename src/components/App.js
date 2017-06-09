b7604cecbdc8710025095a3fc3031d94e2efee96
Raffaele Rizzetti <hello@raffr.co.uk>
2017-06-09 23:55:40 +0100

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-xs-12">
+
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
